db8e3545465e6346619f757c5387eb6555024808
Nutthawut Kueyen <kueyen_n@su.ac.th>
2021-12-11 21:36:59 +0700

    MockupDataUpdate

--- resources/js/pages/cart.vue
+++ resources/js/pages/cart.vue
@@ -17,27 +17,27 @@
     </div>
     <hr class="mt-5" />
     <div class="container">
-      <div class="row row-cols-5">
+      <div class="row row-cols-6">
         <div class="col">รายการสั่งซื้อสินค้า</div>
         <div class="col"></div>
         <div class="col">ราคาต่อหน่วย</div>
         <div class="col">จำนวน</div>
         <div class="col">รายการย่อย</div>
+        <div class="col"></div>
       </div>
       <div class="clearfix mt-5">
         <div class="float-start"><i class="fas fa-store"></i></div>
         <h4>ร้านแม่พันธ์ุไม้ไทย</h4>
       </div>
-      <div v-for="(item, i) in cart" :key="i" class="row row-cols-5 mt-5">
+      <div v-for="(item, i) in cart" :key="i" class="row row-cols-6 mt-5">
         <div class="col">
           <img class="w-100" :src="item.image_url" />
         </div>
-        <div class="col">
-          ต่นเงินไหลมาด่างชมพู
-        </div>
+        <div class="col">ต่นเงินไหลมาด่างชมพู</div>
         <div class="col">{{ item.price }} ฿</div>
         <div class="col">{{ item.amount }}</div>
         <div class="col">{{ item.sum }} ฿</div>
+        <div class="col"><i class="fas fa-trash"></i></div>
       </div>
       <hr />
       <div class="mt-3">
@@ -59,12 +59,10 @@
           </div>
         </div>
 
-        <div class="py-2 px-3 mt-3" style="background: #82C3AB;">
+        <div class="py-2 px-3 mt-3" style="background: #82c3ab">
           <div class="clearfix">
             <div class="float-start">
-              <h3 class="text-white">
-                ยอดสั่งซื้อทั้งหมด
-              </h3>
+              <h3 class="text-white">ยอดสั่งซื้อทั้งหมด</h3>
             </div>
             <div class="float-end">
               <h3 class="text-white">{{ "2,260" }} ฿</h3>
@@ -90,7 +88,7 @@
 
 <script>
 export default {
-  middleware: "auth",
+  middleware: "auth"
   data: () => ({
     cart: [
       {
@@ -128,8 +126,8 @@ export default {
         price: "180",
         amount: "1",
         sum: "790"
-      }
-    ]
+      },
+    ],
   }),
   methods: {
     showAlert() {
@@ -139,12 +137,12 @@ export default {
         showConfirmButton: false,
         timer: 1500
       });
-    }
+    },
   },
 
   metaInfo() {
     return { title: "cart" };
-  }
+  },
 };
 </script>
 
